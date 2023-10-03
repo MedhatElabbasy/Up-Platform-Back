fc5a8f3aa5bbd132104a17e9ce9e36ac8880250e
Hassan Kerdash <Hassan@qutell.com>
2023-10-03 15:10:17 +0200

    Roles

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $user = User::createOrFirst([
+            'name' => 'Super Admin',
+            'email' => 'super_admin@example.com',
+            'email_verified_at' => now(),
+            'password' => 'password'
+        ]);
+
+        $user->assignRole('Super Admin');
+    }
+}
